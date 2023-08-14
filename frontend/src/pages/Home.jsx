7a4961706dfa2d08be8cae9c8eb345e2c45242dc
“8pedro8” <pedrammm@msn.com>
2023-08-14 21:27:40 +0200

    Fetch all workouts and show titles in home + solve CORS issue

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react"
+
 const Home = () => {
+    const [workouts, setWorkouts] = useState(null)
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+          const response = await fetch('http://localhost:4000/api/workouts')
+    
+          if (response.ok) {
+            const json = await response.json()
+            setWorkouts(json)
+            console.log(json)
+          } else {
+            console.error(`Error fetching workouts: ${response.statusText}`);
+          }
+        }
+    
+        fetchWorkouts()
+      }, [])
+
     return ( 
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                { workouts && workouts.map(workout => (
+                    <p key={workout._id}>{ workout.title }</p>
+                ))}
+            </div>
         </div>
      );
 }
