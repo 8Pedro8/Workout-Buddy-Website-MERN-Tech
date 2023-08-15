bb9f8cdbd8e1421da8217dc4321efe960f010b82
“8pedro8” <pedrammm@msn.com>
2023-08-15 09:53:27 +0200

    Create functionality to delete workout from db with request and also in the frontend saved state

--- frontend/src/Components/WorkoutDetails.jsx
+++ frontend/src/Components/WorkoutDetails.jsx
@@ -1,10 +1,26 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+
 const WorkoutDetails = ({ workout }) => {
+    const { dispatch } = useWorkoutsContext()
+
+    const handleClick = async () => {
+        const res = await fetch('http://localhost:4000/api/workouts/' + workout._id, {
+            method: 'DELETE'
+        })
+
+        if (res.ok) {
+            const json = await res.json()
+            dispatch({ type: 'DELETE_WORKOUT', payload: json })
+        }
+    }
+
     return ( 
         <div className="workout-details">
             <h4>{workout.title}</h4>
             <p><strong>Load (kg): </strong> { workout.load }</p>
             <p><strong>Reps: </strong> { workout.reps }</p>
             <p>{ workout.createdAt }</p>
+            <span onClick={handleClick}>delete</span>
         </div>
      );
 }
--- frontend/src/context/WorkoutContext.jsx
+++ frontend/src/context/WorkoutContext.jsx
@@ -12,6 +12,10 @@ export const workoutReducer = (state, action) => {
             return {
                 workouts: [action.payload, ...state.workouts]
             }
+        case 'DELETE_WORKOUT':
+            return {
+                workouts: state.workouts.filter(workout => workout._id !== action.payload._id)
+            }
         default:
             return state
     }
